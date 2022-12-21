87f864ab8bdfebdcca640739ed2479a06c029700
Shashwat Mishra <shashwatmishra91@gmail.com>
2022-12-21 12:32:06 +0530

    initial commit

---
+++ Backend/New2Town/src/main/java/com/FinalYearProject/New2Town/New2TownApplication.java
@@ -0,0 +1,13 @@
+package com.FinalYearProject.New2Town;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class New2TownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(New2TownApplication.class, args);
+	}
+
+}
